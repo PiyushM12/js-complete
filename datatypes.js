75c5e441a3979d90f5d80f4c4e696de143fa8896
PIYUSH MISHRA <piyushhmishraaa@gmail.com>
2024-06-10 23:57:59 +0530

    this is not complete

---
+++ datatypes.js
@@ -0,0 +1,9 @@
+// basically two types primitives and non primitives
+/* primitives same as pass by value
+string, number, boolean ,null ,undefined ,symbol, big int 
+non primitive pass by reference
+array object and functions
+
+js is dynamically typed
+
+*/
