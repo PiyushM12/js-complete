7c0d3a149d0c9e0a0507fcc4c50ef2653610c263
PIYUSH MISHRA <piyushhmishraaa@gmail.com>
2024-06-10 23:18:02 +0530

    read about comparison operators

---
+++ comparison.js
@@ -0,0 +1,10 @@
+// we have some basic comparison apart from those here are some tricky ones
+console.log(null>0) //false
+console.log(null==0) //false
+console.log(null>=0)  //true
+
+
+
+console.log(undefined>0) // gives false in all three cases as above
+// strict check === first checks the datatype and then does the comparison
+
