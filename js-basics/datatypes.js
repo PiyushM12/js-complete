62295139a92816a97e8044d75257cae1ca8b462b
PIYUSH MISHRA <piyushhmishraaa@gmail.com>
2024-06-11 11:44:17 +0530

    stack and heap

--- js-basics/datatypes.js
+++ js-basics/datatypes.js
@@ -28,3 +28,25 @@ let str2 = null;
 console.log(typeof str1) //undefined
 console.log(typeof str2) //object
 //datatype of bigint is undefined
+
+
+//+++++++++++++++++++++++++++++++++++++++++
+// memory allocation
+// stack (primitive), heap (non-primitive)
+// in primitive type we get a copy whereas in non primitive we get the actual reference
+let myname = "piyush"
+let fullname =myname
+fullname="piyush mishra"
+console.log(myname) //piyush
+console.log(fullname) //piyush mishra
+
+//now for non primitive
+let userone={
+  email:"piyush@google.com",
+  upi:"piyush@ibl"
+
+}
+let usertwo= userone
+usertwo.email="piyush@apple.com"
+console.log(userone.email) //piyush@apple.com
+console.log(usertwo.email) //piyush@apple.com
