36b4e8c2082e525d3a5bf1a67811daa3e9d7d0f7
PIYUSH MISHRA <piyushhmishraaa@gmail.com>
2024-06-12 20:31:26 +0530

    date and time

--- js-basics/strings.js
+++ js-basics/strings.js
@@ -3,3 +3,15 @@ let surname= "mishra"
 // ways of string concatination
 console.log(nam + surname)
 console.log(`hello my name is ${nam} and my surname is ${surname}`)
+
+const gamename = new  String("Piyush-pm")  //acts as object
+console.log(gamename.toUpperCase())
+//we have various methods on strings
+const newstr= gamename.substring(0,4);
+console.log(newstr)
+const newstr1= gamename.slice(-3,4);
+console.log(newstr1)
+const url = "https://piyush.com/piyush%20mishra"
+console.log(url.replace("%20","-"))
+// split method to break a string
+// gamename.split(" ")
